204aaee69c1896c7e75829a117fe2014d610e628
Amat <amat7k@gmail.com>
2025-03-03 20:42:47 +0300

    Update  release v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,6 @@
-// "use client"; // اجعل الملف Client Component
-
-// import { useEffect } from "react";
-// import { check } from "@tauri-apps/plugin-updater";
-// import { relaunch } from "@tauri-apps/plugin-process";
-
-// const App = () => {
-//   useEffect(() => {
-//     const updateApp = async () => {
-//       try {
-//         const update = await check();
-//         if (update?.available) {
-//           await update.downloadAndInstall();
-//           await relaunch();
-//         }
-//       } catch (error) {
-//         console.error("Error checking for updates:", error);
-//       }
-//     };
-
-//     if (typeof window !== "undefined") {
-//       updateApp();
-//     }
-//   }, []);
-
-//   return <div>My Tauri App</div>;
-// };
-
-// export default App;
 
 
-"use client"; // اجعل الملف Client Component
+"use client";
 
 import { useEffect } from "react";
 import { check } from "@tauri-apps/plugin-updater";
