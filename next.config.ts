035b36a7f0fb8651f0650f3fd3b12809462fb9b8
Amat <amat7k@gmail.com>
2025-03-04 23:04:28 +0300

    Update  error fronet

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    reactStrictMode: true,  // تأكد من أنك تستخدم الإعدادات الصحيحة
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true, // تأكد من أنك تستخدم الإعدادات الصحيحة
+//   distDir: 'out', // تعيين مجلد الإخراج إلى "out"
+// };
 
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // يضمن تصدير الموقع كملفات HTML ثابتة
+  distDir: "out", // يضع الملفات النهائية في `out/`
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+
