54258f40de936d67f52bbd51447a75a9d628c185
Amat <amat7k@gmail.com>
2025-03-05 16:10:29 +0300

    Update  v4 tes1t

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,33 @@
 
 
+// use tauri_plugin_updater::Builder as UpdaterBuilder;
+
+// fn main() {
+//     tauri::Builder::default()
+//         .setup(|app| {
+//             #[cfg(desktop)] // تأكد من أن هذه الإضافة تم تفعيلها في بيئات سطح المكتب فقط
+//             app.handle().plugin(UpdaterBuilder::new().build())?; // تهيئة Updater بشكل صحيح
+//             Ok(())
+//         })
+//         .run(tauri::generate_context!())
+//         .expect("error while running tauri application");
+// }
+
+use tauri::Manager;
 use tauri_plugin_updater::Builder as UpdaterBuilder;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            #[cfg(desktop)] // تأكد من أن هذه الإضافة تم تفعيلها في بيئات سطح المكتب فقط
-            app.handle().plugin(UpdaterBuilder::new().build())?; // تهيئة Updater بشكل صحيح
+            // التحقق من بيئة سطح المكتب فقط
+            #[cfg(desktop)] 
+            {
+                // تهيئة الـ Updater في بيئة سطح المكتب
+                app.handle().plugin(UpdaterBuilder::new().build())?;
+            }
             Ok(())
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
