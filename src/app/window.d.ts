2a7d988433ec2be4caae5b5091cbd33f83000166
Amat <amat7k@gmail.com>
2025-03-13 01:07:35 +0300

    test v9

--- src/app/window.d.ts
+++ src/app/window.d.ts
@@ -1,5 +1,8 @@
-// src/app/window.d.ts
 interface Window {
-    __TAURI__: any; // أو يمكنك استبدال `any` بنوع أكثر دقة إذا كنت بحاجة لذلك.
-  }
-  
+    __TAURI__: {
+        app: {
+            getVersion: () => Promise<string>;
+            // يمكنك إضافة خصائص أخرى هنا حسب حاجتك
+        };
+    };
+}
