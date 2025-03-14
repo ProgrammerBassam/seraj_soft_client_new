3d6c384d41b70f4b1f61cee0745b2e85e28fec07
Amat <amat7k@gmail.com>
2025-03-14 23:39:52 +0300

    test v11

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,16 +15,21 @@
 
 // use tauri::Manager;
 // src-tauri\src\main.rs
-use tauri_plugin_updater::Builder as UpdaterBuilder;
+use tauri_plugin_updater;
+use tauri_plugin_dialog;
+use tauri_plugin_process;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            // تهيئة الـ Updater
-            app.handle().plugin(UpdaterBuilder::new().build())?;
+            #[cfg(desktop)]
+            app.handle().plugin(tauri_plugin_updater::Builder::new().build())?;
             Ok(())
         })
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_process::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
 
