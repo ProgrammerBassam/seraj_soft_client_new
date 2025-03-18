9dca24581c7d91506c95f2c04a1095e1cd098799
Amat <amat7k@gmail.com>
2025-03-19 00:40:36 +0300

    test v15

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,36 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   reactStrictMode: true, // تأكد من أنك تستخدم الإعدادات الصحيحة
-//   distDir: 'out', // تعيين مجلد الإخراج إلى "out"
+const nextConfig: NextConfig = {
+  reactStrictMode: true, // تأكد من أنك تستخدم الإعدادات الصحيحة
+  output: "export", 
+  distDir: 'out', // تعيين مجلد الإخراج إلى "out"
+};
+
+export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // distDir: "out", // يضع الملفات النهائية في `out/`
+//   reactStrictMode: true,
+//   output: "export", // تأكد من أن هذا الخيار مضبوط
+//   async headers() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Credentials", value: "true" },
+//           { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" }, // تغيير إلى المنفذ المستخدم في Tauri
+//           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type" },
+//         ],
+//       },
+//     ];
+//   },
 // };
 
-// export default nextConfig;
+// module.exports = nextConfig;
 
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // distDir: "out", // يضع الملفات النهائية في `out/`
-  reactStrictMode: true,
-  output: "export",
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" },  // تغيير إلى المنفذ المستخدم في Tauri
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type" },
-        ],
-      },
-    ];
-  },
-
-};
 
 
 
